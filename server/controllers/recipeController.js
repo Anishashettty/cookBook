6bb8b415bdf02211578fb8087e9c577786d933ec
Anishashettty <shettyanisha2004@gmail.com>
2024-10-21 23:18:06 +0530

    add

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,11 +1,65 @@
-
-
-
+require("../modules/database");
+const Category = require("../modules/Category");
 /**
  * GET /
  * Homepage
  */
 
-exports.homepage=async(req,res)=>{
-    res.render('index',{title: 'Cooking blog - Home'});
+exports.homepage = async (req, res) => {
+    try {
+        const limitNumber = 5;
+        const categories=await Category.find({}).limit(limitNumner);
+        res.render('index', { title: 'Cooking Blog - Home', categories, food } );
+    } catch (error) {
+        res.status(500).send({message: error.message || "Error Occured" });
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function insertDymmyCategoryData() {
+//   try {
+//     await Category.insertMany([
+//       {
+//         name: "Thai",
+//         image: "thai-food.jpg",
+//       },
+//       {
+//         name: "American",
+//         image: "american-food.jpg",
+//       },
+//       {
+//         name: "Chinese",
+//         image: "chinese-food.jpg",
+//       },
+//       {
+//         name: "Mexican",
+//         image: "mexican-food.jpg",
+//       },
+//       {
+//         name: "Indian",
+//         image: "indian-food.jpg",
+//       },
+//       {
+//         name: "Spanish",
+//         image: "spanish-food.jpg",
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log("err", +error);
+//   }
+// }
+// insertDymmyCategoryData();
